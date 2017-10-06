7f6833fada9bf7bd43c3b4f4b0d0479abef86b7d
knockinHeavenDoor <61239289@qq.com>
2017-10-06 23:12:46 +0800

    command mode

---
+++ src/command/MyCommand.java
@@ -0,0 +1,14 @@
+package command;
+
+public class MyCommand implements Command {
+
+    private Receiver receiver;
+
+    public MyCommand(Receiver receiver){
+        this.receiver = receiver;
+    }
+    @Override
+    public void exe() {
+        receiver.action();
+    }
+}
---
+++ src/command/Receiver.java
@@ -0,0 +1,11 @@
+package command;
+
+/**
+ * 命令模式
+ */
+public class Receiver {
+
+    public void action(){
+        System.out.println("this action handled");
+    }
+}
