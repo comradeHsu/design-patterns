8927e738a5d4e093bc618792a5fbf922054dc5c4
knockinHeavenDoor <61239289@qq.com>
2017-10-06 23:36:49 +0800

    Memento mode

---
+++ src/memento/Storage.java
@@ -0,0 +1,20 @@
+package memento;
+
+import memento.Memento;
+
+public class Storage {
+
+    private Memento memento;
+
+    public Storage(Memento memento) {
+        this.memento = memento;
+    }
+
+    public Memento getMemento() {
+        return memento;
+    }
+
+    public void setMemento(Memento memento) {
+        this.memento = memento;
+    }
+}
